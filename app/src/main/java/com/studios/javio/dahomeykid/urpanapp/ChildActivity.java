2470ac13120c9cdbb09a0695100bba85de052df4
Jacques <jacques@Macintosh.local>
2017-11-12 15:17:26 +0100

    pass text to activity

--- app/src/main/java/com/studios/javio/dahomeykid/urpanapp/ChildActivity.java
+++ app/src/main/java/com/studios/javio/dahomeykid/urpanapp/ChildActivity.java
@@ -1,5 +1,6 @@
 package com.studios.javio.dahomeykid.urpanapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -14,5 +15,13 @@ public class ChildActivity extends AppCompatActivity {
         setContentView(R.layout.activity_child);
 
         mDisplayText = findViewById(R.id.tv_display);
+
+        Intent intentThatStartedActivity = getIntent();
+
+        if (intentThatStartedActivity.hasExtra(Intent.EXTRA_TEXT)){
+            String textEntered = intentThatStartedActivity.getStringExtra(Intent.EXTRA_TEXT);
+
+            mDisplayText.setText(textEntered);
+        }
     }
 }
--- app/src/main/java/com/studios/javio/dahomeykid/urpanapp/ParentActivity.java
+++ app/src/main/java/com/studios/javio/dahomeykid/urpanapp/ParentActivity.java
@@ -1,5 +1,6 @@
 package com.studios.javio.dahomeykid.urpanapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -17,5 +18,16 @@ public class ParentActivity extends AppCompatActivity {
 
         mDoSomethingCollButton = findViewById(R.id.b_do_something);
         mNameEntry = findViewById(R.id.et_text_entry);
+
+        mDoSomethingCollButton.setOnClickListener((v) -> {
+
+            String enteredText = mNameEntry.getText().toString();
+
+            Intent intent = new Intent(ParentActivity.this, ChildActivity.class);
+
+            intent.putExtra(Intent.EXTRA_TEXT, enteredText);
+
+            startActivity(intent);
+        });
     }
 }
